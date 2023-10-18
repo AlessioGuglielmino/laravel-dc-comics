8e7a6a89ce0a46c7bd3cda31fa6da445c04bb616
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-10-19 01:27:01 +0200

    add_comic_views

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comic;
 use Illuminate\Http\Request;
 
 class ComicsController extends Controller
@@ -13,23 +14,22 @@ class ComicsController extends Controller
      */
     public function index()
     {
-        //
-    }
-
+        $comics= Comic::all();
+        return view('comics.index', compact('comics'));    }
+    };
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return\Illuminate\Http\Response
      */
     public function create()
     {
-        //
-    }
+        return view('comics.create');    };
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -43,10 +43,9 @@ class ComicsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Comic $comic)
     {
-        //
-    }
+        return view('comics.show', compact('comic'));    }
 
     /**
      * Show the form for editing the specified resource.
---
+++ resources/views/comic/index.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('main-content')
+<div class="container">
+
+    
+</div>
+    
+@endsection
