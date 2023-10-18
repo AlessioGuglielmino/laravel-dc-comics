b0f1d9c25e0635d11c3c7cc3fc1da570f8805b58
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-10-18 16:43:09 +0200

    first_commit

---
+++ database/migrations/2023_10_18_141310_create_comics_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->name('title');
+            $table->smallInteger('pages')->unsigned();
+            $table->text('description')->unsigned();
+            $table->enum('genere', ['seinen', 'umorismo', 'horror','azione','fantasy']);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
